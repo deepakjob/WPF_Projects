eed92b9f1f1889c61b9b562155264777232e563f
Deepak Singh <deepak.singh6765@gmail.com>
2024-05-06 16:18:47 +0530

    Added Code

---
+++ EmployeeManagement/Commands/RelayCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace EmployeeManagement.Commands
+{
+    internal class RelayCommand:ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        private Action DoWork;
+
+        public RelayCommand(Action doWork)
+        { this.DoWork = doWork; }
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            DoWork();
+        }
+    }
+}
